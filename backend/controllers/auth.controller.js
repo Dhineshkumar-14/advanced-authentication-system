d58a02a95cc6a0213b4f882f64cf5ecf76919d84
Dhineshkumar <dhineshkumar.velayudham@gmail.com>
2025-11-23 00:23:37 +0530

    Completed signup routes

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,4 +1,54 @@
-export const signup = async (req, res) => {};
+import { User } from "../models/user.model.js";
+import bcryptjs from "bcryptjs";
+import { generateTokenAndSetCookie } from "../utils/generateTokenAndSetCookie.js";
+
+export const signup = async (req, res) => {
+  try {
+    const { email, name, password } = req.body;
+    if (!email || !name || !password) {
+      return res
+        .status(400)
+        .json({ message: "All fields are Required", success: false });
+    }
+
+    const userAlreadyExists = await User.findOne({ email });
+
+    if (userAlreadyExists) {
+      return res
+        .status(400)
+        .json({ message: "User already exists", success: false });
+    }
+    const hashedPassword = await bcryptjs.hash(password, 10);
+    const verificationToken = Math.floor(
+      100000 + Math.random() * 900000
+    ).toString();
+    const user = new User({
+      name,
+      email,
+      password: hashedPassword,
+      verificationToken,
+      verificationTokenExpiresAt: Date.now() + 24 * 60 * 60 * 1000,
+    });
+
+    await user.save();
+
+    generateTokenAndSetCookie(res, user._id);
+
+    return res.status(201).json({
+      message: "User created successfully",
+      success: false,
+      user: {
+        ...user._doc,
+        password: undefined,
+      },
+    });
+  } catch (error) {
+    console.log("Error in signup" + error);
+    return res
+      .status(500)
+      .json({ message: "Internal sever exception", sucess: false });
+  }
+};
 
 export const login = async (req, res) => {};
 
--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,11 @@
 import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./lib/db.js";
-import authRouter from "./routes/auth.routh.js";
+import authRouter from "./routes/auth.route.js";
 
 const app = express();
 dotenv.config();
+app.use(express.json());
 const PORT = process.env.PORT || 5000;
 
 app.use("/auth", authRouter);
